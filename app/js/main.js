9548bd73c7810f526c11c7261dae4d0553155cf0
Morten Nielsen <morten@morkeleb.com>
2013-05-12 21:58:57 +0200

    added angular app to show warnings and hearts and what not

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+angular.module('ekg', []).
+  config(function($routeProvider) {
+    $routeProvider.
+      when('/', {controller:ListCtrl, templateUrl:'templates/list.html'}).
+      when('/organizations/:organization', {controller:OrganCtrl, templateUrl:'templates/organization.html'}).
+      otherwise({redirectTo:'/'});
+  });
+
+function ListCtrl ($scope, $http) {
+	$scope.organizations = [];
+	$scope.organizations.push({name:'Remotex', id:1});
+}
+
+function OrganCtrl ($scope) {
+	$scope.name = "RemoteX";
+}
+
