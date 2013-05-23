c75534314318e38403c22a710a08d13edf0f1d8d
Morten Nielsen <morten@morkeleb.com>
2013-05-23 22:34:54 +0200

    adding functionality for sending an invitation email

--- models/organization.py
+++ models/organization.py
@@ -1,5 +1,7 @@
 from google.appengine.ext import db
 from models.heart import Heart
+from google.appengine.api import users
+from models import invite_mail
 
 
 class Organization(db.Model):
@@ -8,3 +10,24 @@ class Organization(db.Model):
 
     def get_heart(self, id):
         return Heart.get_or_insert(id, parent=self)
+
+    def send_invitation(self, email):
+        invi = Invitation(parent=self)
+        invi.email = email
+        invi.put()
+        invi.send()
+
+
+class Invitation(db.Model):
+    email = db.StringProperty()
+
+    def send(self):
+        invite_mail(self)
+
+    def accept(self):
+        self.parent().users.push(users.get_current_user().email())
+        self.parent().put()
+        self.delete()
+
+    def decline(self):
+        self.delete()
