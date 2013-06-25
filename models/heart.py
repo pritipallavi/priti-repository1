7d0d1cf763cb4c17c9ae182ff4932f4c69aeb2f1
Morten Nielsen <morten@morkeleb.com>
2013-06-25 09:57:15 +0200

    allowing a cron schema and check it if we have it

--- models/heart.py
+++ models/heart.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import db
 import datetime
 from models import resuscitate_mail, flatline_mail
+from lib.croniter import croniter
 
 
 class Heart(db.Model):
@@ -8,6 +9,7 @@ class Heart(db.Model):
     created = db.DateTimeProperty(auto_now_add=True)
     last_pulse = db.DateTimeProperty(auto_now_add=True)
     threshold = db.IntegerProperty(default=0)
+    cron = db.StringProperty(default='')
 
     def registerPulse(self):
         flatline = self.getActiveFlatline()
@@ -24,12 +26,17 @@ class Heart(db.Model):
         if active is not None:
             return
 
-        if self.last_pulse + datetime.timedelta(seconds=self.threshold*2) > datetime.datetime.now():
+        if not self.is_flatlined():
             return
         f = Flatline(parent=self)
         f.start = self.last_pulse
         f.put()
         flatline_mail(self)
+
+    def is_flatlined(self):
+        if self.cron != '':
+            return croniter(self.cron, self.last_pulse).get_next(datetime) + datetime.timedelta(seconds=self.threshold) < datetime.datetime.now()
+        return self.last_pulse + datetime.timedelta(seconds=self.threshold*2) < datetime.datetime.now()
 
 
 class Flatline(db.Model):
